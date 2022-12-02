bd1b11af864d2814a6f6e7f356e239bc6f166882
Willard Nilges <wdn5796@rit.edu>
2022-12-02 01:18:35 -0500

    Holy fucking shit it works

--- DevcadeGame/AEObject.cs
+++ DevcadeGame/AEObject.cs
@@ -1,4 +1,4 @@
-using DevcadeGame;
+﻿using DevcadeGame;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -8,32 +8,27 @@ using tainicom.Aether.Physics2D.Dynamics.Contacts;
 
 namespace WraithHunt
 {
-    public class AEPlayer : AEObject
+    public class AEObject
     {
-        // Health
-        public int healthMax = 10;
-        public int health = 10;
-
-        // Movement
-        private float _moveSpeed = 1.0f;
-        private bool _hasJumped = true;
-        private float _jumpPower = 15.0f;
-
-        // Planar nonsense
-        public Plane currentPlane;
+        protected string _spritePath;
+        protected Texture2D _sprite;
+        protected float _spriteScale;
+        protected Body _body;
+        public Vector2 BodySize;
 
         /// <summary>
         /// A boolean indicating if this ball is colliding with another
         /// </summary>
         public bool Colliding { get; protected set; }
 
-        public AEPlayer(string spritePath, float spriteScale, Vector2 bodySize, Body body) : base(spritePath, spriteScale, bodySize, body)
+        public AEObject(string spritePath, float spriteScale, Vector2 bodySize, Body body)
         {
             this._spritePath = spritePath;
             this._spriteScale = spriteScale;
             this.BodySize = bodySize;
             this._body = body;
             _body.Mass = 1;
+            _body.SetFriction(0.75f);
 
             this._body.OnCollision += CollisionHandler;
             this._body.FixedRotation = true;
@@ -41,57 +36,57 @@ namespace WraithHunt
 
         /**** FUN STUFF ****/
 
-        public void Walk(Direction direction)
-        {
-            switch (direction)
-            {
-                case Direction.LEFT:
-                    _body.ApplyLinearImpulse(new Vector2(-1 * _moveSpeed, 0));
-                    break;
-                case Direction.RIGHT:
-                    _body.ApplyLinearImpulse(new Vector2(_moveSpeed, 0));
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        public void Jump()
-        {
-            if (!_hasJumped)
-            {
-                _body.ApplyLinearImpulse(new Vector2(0, -1 * _jumpPower));
-                _hasJumped = true;
-            }
-        }
+        // Nothing, bozo.
 
         /**** DATA ****/
 
+        public Vector2 Position() => _body.Position;
+
         /**** MONOGAME PLUMBING ****/
+
+        /// <summary>
+        /// Loads the ball's texture
+        /// </summary>
+        /// <param name="contentManager">The content manager to use</param>
+        public void LoadContent(ContentManager contentManager)
+        {
+            _sprite = contentManager.Load<Texture2D>(_spritePath);
+        }
+
+        /// <summary>
+        /// Updates the ball
+        /// </summary>
+        /// <param name="gameTime">An object representing time in the game</param>
+        public void Update(GameTime gameTime)
+        {
+            // Clear the colliding flag 
+            Colliding = false;
+            _body.Rotation = 0;
+        }
 
         /// <summary>
         /// Draws the ball using the provided spritebatch
         /// </summary>
         /// <param name="gameTime">an object representing time in the game</param>
         /// <param name="spriteBatch">The spritebatch to render with</param>
-        /*public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(
                 _sprite,
                 new Rectangle(
-                    (int)((_body.Position.X * _spriteScale)),
-                    (int)((_body.Position.Y * _spriteScale)),
+                    (int)((_body.Position.X * _spriteScale) - (BodySize.X * _spriteScale) / 2.0f),
+                    (int)((_body.Position.Y * _spriteScale) - (BodySize.Y * _spriteScale) / 2.0f),
                     (int)(BodySize.X * _spriteScale),
                     (int)(BodySize.Y * _spriteScale)
                 ),
                 Color.White
             );
-        }*/
+        }
+
 
         bool CollisionHandler(Fixture fixture, Fixture other, Contact contact)
         {
             Colliding = true;
-            _hasJumped = false;
             return true;
         }
 
